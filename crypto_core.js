5f70bf553e31a13cbd188e88b766f9ddb3b8976e
matveisem4-dot <matveisem4@gmail.com>
2026-04-09 15:42:53 +0200

    Create crypto_core.js

---
+++ crypto_core.js
@@ -0,0 +1,24 @@
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+
+// Ключ берем из переменных окружения (Settings -> Secrets в GitHub)
+const ENCRYPTION_KEY = process.env.DB_SECRET_KEY || 'default_secret_32_chars_long_!!!'; 
+const IV_LENGTH = 16;
+
+function encrypt(text) {
+    let iv = crypto.randomBytes(IV_LENGTH);
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+function decrypt(text) {
+    let textParts = text.split(':');
+    let iv = Buffer.from(textParts.shift(), 'hex');
+    let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
